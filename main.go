139b22e18d46302d4b6d355c7bee62c978ae103c
Tommy Dyer <tommydyer@Tommys-MBP.lan>
2021-04-26 11:55:42 -0400

    redoing homework from assinged after first session

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-
+	// original
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	// #1
+	r.GET("/friends", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"array": [4]int{1, 2, 3, 4},
+		})
+	})
+
+	// #2
+	r.GET("/name", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hi! I am Thomas.",
+		})
+	})
+
+	// #3
+	r.GET("/birthday", func(c *gin.Context) {
+		c.JSON(201, gin.H{
+			"message": "",
+		})
+	})
+
+	r.POST("/friends", func(c *gin.Context) {
+		body, _ := ioutil.ReadAll(c.Request.Body)
+		println(string(body))
 	})
 
 	r.Run()
